6c34dd4fd9cfce1188499cef52cc8d21ccdd1377
lfelipelopes <lfelipelopesti@gmail.com>
2022-09-07 17:50:40 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+import { DashBoard } from './src/screens/DashBoard';
+import AppLoading from 'expo-app-loading';
+
+export default function App(): JSX.Element {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <DashBoard />
+    </ThemeProvider>
+  );
+}
+
